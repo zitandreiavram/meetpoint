352c3db11da12345fd053357cd1a05f580fb2c50
Andrei <zit_andreiavram@yahoo.com>
2013-01-07 15:05:51 +0200

    js html

--- js/main.js
+++ js/main.js
@@ -46,6 +46,15 @@ jQuery(document).ready(function($) {
 		localStorage.removeItem('_app_data');
 	})
 	
+	$('#notification').bind('click', function() {
+		navigator.notification.confirm('Message...', confirmCallback, 'Chat?', 'YES,NO');
+		return false;
+	})
+	
+	function confirmCallback(buttonIndex) {
+		alert('You selected button ' + buttonIndex);
+	}
+	
 	$('.send_message_open').live('click', function() {
 		$('#to').text($(this).attr('rel'));
 		$('#send_message').show();
@@ -62,14 +71,6 @@ jQuery(document).ready(function($) {
 	})
 
 })
-
-function showConfirm() {
-	navigator.notification.confirm(message, confirmCallback, 'Chat?', 'YES,NO');
-}
-
-function confirmCallback(buttonIndex) {
-	alert('You selected button ' + buttonIndex);
-}
 
 function map() {
 	$('#map_canvas').gmap().bind('init', function(evt, map) {
@@ -162,15 +163,3 @@ var getLocation = function() {
     navigator.geolocation.getCurrentPosition(suc, locFail);
 };
 
-function showAlert() {
-    navigator.notification.alert(
-        'You are the winner!',  // message
-        alertDismissed,         // callback
-        'Game Over',            // title
-        'Done'                  // buttonName
-    );
-}
-
-function alertDismissed() {
-    alert(1);
-}
