a6bd9216d445deb39b534add5f58bf3662f8a0c4
Andrei <zit_andreiavram@yahoo.com>
2013-01-09 12:16:11 +0200

    die all

--- app/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$db_link = mysql_connect($config['database'][ENV]['hostname'], $config['database'][ENV]['username'], $config['database'][ENV]['password'])
-	or die(mysql_error());
-
-mysql_select_db($config['database'][ENV]['database']) or die(mysql_error());
--- app/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'simpleimage.php';
-ini_set('memory_limit', '1024M');
-$image = new SimpleImage();
-$image->load('uploads/47c9f956828a8df9e557954ef687e27f.jpg');
-$image->resizeToWidth(250);
-$image->save('uploads/47c9f956828a8df9e557954ef687e27f.jpg');
